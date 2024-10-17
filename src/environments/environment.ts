2d56993d77017cb42a9acc7a2f6e44dc6d821d00
Juan Carlos <juancarlospl7312@gmail.com>
2024-10-17 14:24:31 -0400

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,13 +3,13 @@ export const environment = {
     environmentName: "ThisFeelsRightAIDemo",
 
     // Hardcoded API keys (replace these with real values)
-    // googleApiKey: "AIzaSyDg6_dfOSOyJM8PpzmafDUG7He9k_5SgSY",
-    // cseId: "e6eeb36a1359c42ff",
+    googleApiKey: "googleApiKey",
+    cseId: "cseId",
     googleApiURl: "https://www.googleapis.com/customsearch/v1",
     // Scraper API key
-    // scraperApiKey: "6d599e976d6915f50dff4f9f4f9103d0",
+    scraperApiKey: "scraperApiKey",
     scraperApiUrl: "https://api.scraperapi.com",
     // OpenAI API key
-    // openAiApiKey: "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",
+    openAiApiKey: "openAiApiKey",
     apiUrl: "https://api.openai.com/v1/chat/completions",
 }
