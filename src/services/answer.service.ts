6cbc57c88031f32c161c446cc8e9d90fe4755c6d
Juan Carlos <juancarlospl7312@gmail.com>
2024-10-17 16:43:36 -0400

    updates

--- src/services/answer.service.ts
+++ src/services/answer.service.ts
@@ -28,7 +28,7 @@ export class AnswerService {
 
         const processedSources$ = sources.map(({ title, link }) =>
             {
-                delay(3000);
+                delay(10000);
                 return this.searchService.fetchPageContent(link).pipe(
                     switchMap((htmlContent) => {
                         if (!htmlContent || htmlContent.length < 250) return of(null);
