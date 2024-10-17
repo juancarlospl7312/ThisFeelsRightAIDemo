ceaac5b1ac5ebcd6353aae22735361b8fd4f7cd0
Juan Carlos <juancarlospl7312@gmail.com>
2024-10-17 14:53:50 -0400

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,13 +3,13 @@ export const environment = {
     environmentName: "ThisFeelsRightAIDemo",
 
     // Hardcoded API keys (replace these with real values)
-    googleApiKey: process.env['googleApiKey'],
-    cseId: process.env['cseId'],
+    googleApiKey: process.env.googleApiKey,
+    cseId: process.env.cseId,
     googleApiURl: "https://www.googleapis.com/customsearch/v1",
     // Scraper API key
-    scraperApiKey: process.env['scraperApiKey'],
+    scraperApiKey: process.env.scraperApiKey,
     scraperApiUrl: "https://api.scraperapi.com",
     // OpenAI API key
-    openAiApiKey: process.env['openAiApiKey'],
+    openAiApiKey: process.env.openAiApiKey,
     apiUrl: "https://api.openai.com/v1/chat/completions",
 }
