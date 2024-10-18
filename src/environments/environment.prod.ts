3a894f7e9e667b84233a32a7df6e61cdd65f682d
Juan Carlos <juancarlospl7312@gmail.com>
2024-10-18 04:21:19 -0400

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,6 @@
 export const environment = {
     GOOGLE_API_KEY: '',
     CSE_ID: '',
-    GOOGLE_API_URL: '',
     SCRAPER_API_KEY: '',
-    SCRAPER_API_URL: '',
     OPENAI_API_KEY: '',
-    OPENAI_API_URL: '',
 };
